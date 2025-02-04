aa49479a98e98d014e9d9d6b5e5c572c5e6e5427
GiovanniVinciguerra <giovy.vinci@gmail.com>
2025-02-04 20:39:55 +0100

    Completata finestra signig, creato primo post request per la memorizzazione dei dati utente

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("clickMe")
-
-if (button) {
-    button.addEventListener("click",() => {
-        window.location.href = "home.html"
-    })
-}
--- src/signin.ts
+++ src/signin.ts
@@ -1,2 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import axios from "axios"
+
+const button = document.getElementById("submit")
+const name_input = document.getElementById("name") as HTMLInputElement
+const surname_input = document.getElementById("surname") as HTMLInputElement
+const username_input = document.getElementById("username") as HTMLInputElement
+const email_input = document.getElementById("email") as HTMLInputElement
+const password_input = document.getElementById("password") as HTMLInputElement
+
+if(button) {
+    button.addEventListener("click", async () => {
+        if(name_input.value != null && surname_input.value != null && username_input.value != null && email_input.value != null && password_input.value != null) {
+            try {
+                const response = await axios.post('http://urlserver/signin', {
+                    nome: name_input.value,
+                    surname: surname_input.value,
+                    username: username_input.value,
+                    email: email_input.value,
+                    password: password_input.value
+                });
+                console.log('Response:', response.data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    })
+}
