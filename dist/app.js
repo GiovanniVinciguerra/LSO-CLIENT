b84115d6af0e7a60a17c80a3a7cab811ceeb228e
GiovanniVinciguerra <giovy.vinci@gmail.com>
2025-04-16 19:01:42 +0200

    Cambio linguaggio client

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const button = document.getElementById("clickMe");
-if (button) {
-    button.addEventListener("click", () => {
-        window.location.href = "home.html";
-    });
-}
